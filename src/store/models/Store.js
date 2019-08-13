d13ffc95bd22d95c5b1028f74b8a6deaaae9285d
fila90 <ivan.filipovic.fila@gmail.com>
2019-08-13 10:56:06 +0200

    add authUser function rename checkUser method

--- src/store/models/Store.js
+++ src/store/models/Store.js
@@ -1,21 +1,32 @@
 import { types, flow } from "mobx-state-tree"
 import User from './User'
-import { checkSession } from '../../api'
+import { checkSession, authUser } from '../../api'
 
 const Store = types
   .model({
     user: types.maybeNull(User)
   })
   .actions(self => ({
-    checkUser: flow(function*() {
+    checkSession: flow(function*() {
       try {
         const res = yield checkSession();
         if (res.success) {
-          self.setUser(res.data)
-          return true
-        } else throw Error('no auth')
+          self.setUser(res.data);
+          return true;
+        } else throw Error('no auth');
       } catch (err) {
-        return false
+        return false;
+      }
+    }),
+    authUser: flow(function* (code) {
+      try {
+        const res = yield authUser(code);
+        if(res.success) {
+          self.setUser(res.data);
+          return true;
+        } else throw Error('code not valid');
+      } catch (err) {
+        return false;
       }
     }),
     setUser(user) {
