00f8e664ad79914629e0955845f45188ccb48cec
fila90 <ivan.filipovic.fila@gmail.com>
2019-08-19 16:59:04 +0200

    add conditional rendering for projects route

--- src/views/MainProjects.jsx
+++ src/views/MainProjects.jsx
@@ -2,19 +2,23 @@ import React, { useContext } from 'react'
 import { observer } from 'mobx-react';
 import Store from '../store';
 import ProjectNew from '../components/ProjectNew'
+import ProjectDetails from '../components/ProjectDetails'
 
 function MainProjects({ match }) {
   const store = useContext(Store)
   const { id } = match.params
   const { projects } = store
+  const project = id && projects && projects.find(p => p.id == id)
 
   return (
-    <div className="project-list">
-      {projects
-        ? projects.map(p => <ProjectNew key={p.id} project={p} />)
-        : <h1 className="h__title">just a sec...</h1>
-      }
-    </div>
+    project
+    ? <ProjectDetails project={project} />
+    : <div className="project-list">
+        {projects
+          ? projects.map(p => <ProjectNew key={p.id} project={p} />)
+          : <h1 className="h__title">just a sec...</h1>
+        }
+      </div>
   )
 }
 
